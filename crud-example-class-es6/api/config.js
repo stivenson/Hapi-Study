ceb248bc68ee7090bc3605e192c158dacabcfc8c
Stivenson Rincon Mora <stivenson@Stivensons-MacBook-Air.local>
2017-04-29 00:06:02 -0500

    proyect crud-example-class-es6 - list resources and general Structure

---
+++ crud-example-class-es6/api/config.js
@@ -0,0 +1,15 @@
+var Config = {
+	plugins : [
+	    {
+	        register: require('hapi-auth-jwt2')
+	    },
+	    {
+	        register: require('hapi-authorization'),
+	        options: {
+	          roles: ['ADMIN', 'MANAGER', 'EMPLOYEE']  // By setting to false, you are not using an authorization hierarchy and you do not need to specify all the potential roles here 
+	        }
+	    }
+	]
+}
+
+module.exports = Config;
